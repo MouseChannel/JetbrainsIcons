ac856231bfd971dc0a1e0cc7b06fedf9094fcc17
mousechannel <mochenghh@gmail.com>
2025-02-11 09:38:11 +0800

    add icon file
    (cherry picked from commit 8453de9f53b74ad0a4d92848d9faf9e029370cf6)

--- generate/scripts/generate.ts
+++ generate/scripts/generate.ts
@@ -23,7 +23,7 @@ function generateIcons(variant: string) {
 
 // VSCode sometimes doesn't need the `fileExtension` block to be populated but that affects JetBrains so we can add our
 // own extensions/overrides here if need be.
-const customFileExtensions = {razor: "razor"}
+// const customFileExtensions = {razor: "razor"}
 // const extendedFileExtensions = {...fileExtensions, ...customFileExtensions}
 
 let folder_icons = folderIcons[0]['icons']
@@ -71,16 +71,12 @@ class Icons(private val variant: String) {`
 
   Object.entries(file_icons).forEach(([k, v]) => {
     let name = v['name'];
-    if (v['fileExtensions'])
-      for (let index = 0; index < v['fileExtensions'].length; index++) {
-        let name_target = v['fileExtensions'][index]
-        data += `        "${name_target}" to ${name.replaceAll('-', '_')},\n`
-      }
+
     if (v['fileNames']) {
 
       for (let index = 0; index < v['fileNames'].length; index++) {
         let name_target = v['fileNames'][index]
-        // data += `        "${name_target}" to ${name.replaceAll('-', '_')},\n`
+        data += `        "${name_target}" to ${name.replaceAll('-', '_')},\n`
       }
     }
   })
